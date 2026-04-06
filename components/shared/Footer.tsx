c511dff199e452ce4a6f77e4d87e15bd7fe7a2b0
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-04-06 18:05:48 +0500

    Hero UI completed

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex flex-between flex-col gap-4 p-5 text-center sm:flex-row'>
+
+        <Link href="/">
+        <Image src="/assets/images/logo.png" alt="Logo" width={40} height={40}/>
+        </Link>
+
+        <p>2026 Eventify. All Rights reserved.</p>
+
+
+      </div>
+    </footer>
   )
 }
 
