790bb044df858793b8e142d0d816792fcb77dc36
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-04-07 14:13:50 +0500

    user model added

---
+++ lib/mongodb/database/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model, models } from "mongoose"
+
+const UserSchema = new Schema({
+    clerkId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { Type: String, required: true },
+    photo: { Type: String, required: true },
+})
+
+
+const User = models.User || model('User', UserSchema);
+
+
+
+export default User;
