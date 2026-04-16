d8bcbb8ae6b08b270fab54071a5150b50ccb4d14
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-04-16 10:31:53 +0500

    Update the user model to remove the required from some fields for testing purpose

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -8,10 +8,10 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
  
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const CLERK_WEBHOOK_SIGNING_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
  
-  if (!CLERK_WEBHOOK_SIGNING_SECRET) {
-    throw new Error('Please add CLERK_WEBHOOK_SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+  if (!WEBHOOK_SECRET) {
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
  
   // Get the headers
@@ -32,7 +32,7 @@ export async function POST(req: Request) {
   const body = JSON.stringify(payload);
  
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(CLERK_WEBHOOK_SIGNING_SECRET);
+  const wh = new Webhook(WEBHOOK_SECRET);
  
   let evt: WebhookEvent
  
