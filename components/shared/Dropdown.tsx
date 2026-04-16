e3d09f62f95c91dd8d82c3bcdc2b3be41d721754
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-04-16 21:24:33 +0500

    Add Dropdown EventForm alertdialog,form,input,label,select and validator.ts for Shadcn Schemas

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
+import { ICategory } from '@/lib/mongodb/database/models/category.model'
+import { Input } from '../ui/input'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+
+
+
+type DropdownProps = {
+    value?: string,
+    onChangeHandler?: () => void
+}
+
+const Dropdown = ({ value, onChangeHandler}: DropdownProps) => {
+
+    const [categories, setCategories] = useState<ICategory[]>([]);
+
+  return (
+    <>
+      <Select onValueChange={onChangeHandler} defaultValue={value}>
+
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+
+
+      <SelectContent>
+
+
+        {categories.length > 0 && categories.map((category) => (
+            <SelectItem key={category._id} value={category._id}>
+                {category.name}
+            </SelectItem>
+        ))}
+
+
+
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Add new category</AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input type="text" placeholder="Category name" className="input-field mt-3"/>
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Add</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+
+
+      </SelectContent>
+
+
+      </Select>
+    </>
+  )
+}
+
+export default Dropdown
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,5 +1,17 @@
 "use client"
 
+import { eventDefaultValues } from "@/constants";
+import { eventFormSchema } from "@/lib/validator";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Form, FormItem, FormMessage, FormField, FormControl } from "../ui/form";
+import { Input } from "../ui/input";
+import Dropdown from "./Dropdown";
+import { Button } from "../ui/button";
+
+
+
 
 type EventFormProps = {
     userId: string;
@@ -7,9 +19,53 @@ type EventFormProps = {
 }
 
 const EventForm = ({userId, type}: EventFormProps) => {
+
+  const initialValues = eventDefaultValues
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues
+  })
+
+  
   return (
     <>
-      
+      <Form {...form}>
+        
+        <form className="flex flex-col gap-5">
+          <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Event title" {...field} className="input-field" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+
+        <Button type="submit" size="lg" className="button col-span-2 w-full">
+          Submit
+        </Button>
+        </form>
+      </Form>
     </>
   )
 }
