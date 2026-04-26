e08f81588966a3d88822ded376acee25a559ae82
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-04-26 10:48:15 +0500

    Add Another function of populateEvent to Get the organizer and category details not just id when fetching data from the database through API

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,13 +5,20 @@ import { handleError } from "../utils";
 import Event from "../mongodb/database/models/event.model"
 import User from "../mongodb/database/models/user.model"
 import { CreateEventParams } from "@/types";
+import Category from "../mongodb/database/models/category.model";
+
+
+const populateEvent = async (query: any) => {
+    return query.populate({ path: 'organizer', model: User, select: '_id firstName lastName'})
+    .populate({ path: 'category', model: Category, select: '_id name'});
+}
 
 export const CreateEvent = async ({event, userId, path}: CreateEventParams) => {
     try {
         
         await connectToDatabase();
 
-        const organizer = await User.findById(userId);
+        const organizer = await populateEvent(User.findById(userId));
 
         if(!organizer){
             throw new Error("Organizer not found");
@@ -37,7 +44,7 @@ export const getEventById = async (eventId: string) => {
     try {
         await connectToDatabase();
 
-        const event = await Event.findById(eventId);
+        const event = await populateEvent(Event.findById(eventId));
 
 
         if(!event){
@@ -46,8 +53,9 @@ export const getEventById = async (eventId: string) => {
 
         return JSON.parse(JSON.stringify(event));
 
-        
+
     } catch (error) {
         handleError(error);
     }
 }
+
