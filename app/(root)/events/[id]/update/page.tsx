4f82038a784b9049d18eed00ff297b0bf1447fe8
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-05-01 22:35:32 +0500

    Add Update Event Functionality

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,12 +1,21 @@
 import EventForm from '@/components/shared/EventForm'
+import { getEventById } from '@/lib/actions/event.actions';
+import { UpdateEventParams } from '@/types';
 import { auth } from '@clerk/nextjs'
 import React from 'react'
 
-const UpdateEvent = () => {
+type UpdateEventProps = UpdateEventParams & {
+  params: {
+    id: string
+  }
+}
+
+const UpdateEvent = async ({params: { id } }: UpdateEventProps) => {
 
     const { sessionClaims } = auth();
 
     const userId = sessionClaims?.userId as string;
+    const event = await getEventById(id);
 
 
   return (
@@ -16,14 +25,12 @@ const UpdateEvent = () => {
       </section>
 
       <div className='wrapper my-8'>
-        <EventForm userId={userId} type="Update"/>
+        <EventForm userId={userId} type="Update" event={event} eventId={event._id}/>
       </div>
     </>
   )
 }
 
 export default UpdateEvent
-
-
 
 
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -18,19 +18,28 @@ import DatePicker from "react-datepicker";
 import { Checkbox } from "../ui/checkbox";
 import { useUploadThing } from "@/lib/uploadthing";
 import { useRouter } from "next/navigation";
-import { CreateEvent } from "@/lib/actions/event.actions";
+import { createEvent, updateEvent } from "@/lib/actions/event.actions";
+import { IEvent } from "@/lib/mongodb/database/models/event.model";
 
 
 
 type EventFormProps = {
     userId: string;
-    type: "Create" | "Update"
+    type: "Create" | "Update",
+    event?: IEvent,
+    eventId?: string
 }
 
-const EventForm = ({userId, type}: EventFormProps) => {
+const EventForm = ({userId, type, event, eventId}: EventFormProps) => {
 
-  const [files, setFiles] = useState<File[]>([])
-  const initialValues = eventDefaultValues
+  const [files, setFiles] = useState<File[]>([]);
+
+  const initialValues = event && type === "Update" ? {
+    ...event,
+    startDateTime: new Date(event.startDateTime),
+    endDateTime: new Date(event.endDateTime)
+  } : eventDefaultValues;
+
   const router = useRouter();
 
   const { startUpload } = useUploadThing("imageUploader");
@@ -64,7 +73,7 @@ const EventForm = ({userId, type}: EventFormProps) => {
 
       try {
 
-        const newEvent = await CreateEvent({
+        const newEvent = await createEvent({
           event: {...values, imageUrl: uploadedImageUrl},
           userId,
           path: '/profile'
@@ -81,6 +90,33 @@ const EventForm = ({userId, type}: EventFormProps) => {
       }
 
 
+    }
+
+    if(type === "Update"){
+
+      if(!eventId){
+        router.back();
+        return;
+      }
+
+      try {
+
+
+        const updatedEvent = await updateEvent({
+          userId,
+          event: {...values, imageUrl: uploadedImageUrl, _id: eventId},
+          path: `/events/${eventId}`
+        });
+
+
+        if(updatedEvent){
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`);
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
     }
 
 
